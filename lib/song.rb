91a9ca9e281376e402aed7e5841a34091597aea5
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-08-24 20:19:20 -0500

    commit tree f17e17ac4ea8589af8398677cf231d3e8bc2ed1a

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
 class Song
     attr_accessor :title, :artist
 
-
-
-
-
 end
+
+# song1 = Song.new("Despacito")
+# song1.artist = "Luis fonsi"
+# song1.artist.genre = "regatton"
+# song1.artist.name 
+# Song.methods
+# Song.instance_methods
